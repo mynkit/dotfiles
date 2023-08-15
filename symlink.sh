f833951f7205e054a548183ffaab700ec01b776d
mynkit <dcm5124289@gmail.com>
2023-08-16 08:41:48 +0900

    update symlink

--- symlink.sh
+++ symlink.sh
@@ -1,3 +1,5 @@
 # init.vim (nvim)
+mkdir -p "$HOME/.config/nvim/init.vim"
 ln -s "$PWD/dotfiles/init.vim" "$HOME/.config/nvim/init.vim"
 ln -s "$PWD/startup.scd" "$HOME/Library/Application Support/SuperCollider/startup.scd"
+ln -s "$PWD/BootTidal.hs" "$HOME/BootTidal.hs"
