b87817ae2de68e05e1d688c7c428f32b1a53e754
keita miyano <keitamiyano@keitanombp.lan>
2024-08-02 12:26:49 +0900

    fix

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,6 @@
 # init.vim (nvim)
 mkdir -p "$HOME/.config/nvim/init.vim"
-ln -s "$PWD/dotfiles/init.vim" "$HOME/.config/nvim/init.vim"
+ln -s "$PWD/init.vim" "$HOME/.config/nvim/init.vim"
 ln -s "$PWD/startup.scd" "$HOME/Library/Application Support/SuperCollider/startup.scd"
 ln -s "$PWD/BootTidal.hs" "$HOME/BootTidal.hs"
 ln -s "$PWD/.zshrc" "$HOME/.zshrc"
