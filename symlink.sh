60cea537892eac10bab33c8c9e373a169dc0d479
mynkit <dcm5124289@gmail.com>
2026-08-13 10:43:08 +0900

    add symlink (menubar command)

--- symlink.sh
+++ symlink.sh
@@ -4,3 +4,7 @@ ln -s "$PWD/init.vim" "$HOME/.config/nvim/init.vim"
 ln -s "$PWD/startup.scd" "$HOME/Library/Application Support/SuperCollider/startup.scd"
 ln -s "$PWD/BootTidal.hs" "$HOME/BootTidal.hs"
 ln -s "$PWD/.zshrc" "$HOME/.zshrc"
+
+# 自作コマンド (.zshrc で $HOME/.local/bin に PATH が通っている)
+mkdir -p "$HOME/.local/bin"
+ln -s "$PWD/bin/menubar" "$HOME/.local/bin/menubar"
